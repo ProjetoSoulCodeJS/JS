4445bfa515578ecc854d35fa2d61a4543b764d69
Gustavo <gustavonew2011@gmail.com>
2021-03-16 19:49:24 -0300

    calculadora atualização

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,26 @@
+var valores = []
+
+function result(n){
+valores += (parseInt(document.getElementById('resultado').value = n))
+console.log(valores)
+}
+
+function soma(n){
+valores = valores + '+'
+}
+
+function subtrair(n){
+valores = valores + '-'
+}
+
+function mult(n){
+valores = valores + '*'
+}
+
+function div(n){
+valores = valores + '/'
+}
+
+
+
+
