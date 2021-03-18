9340ffe42f2c0c4b9b823d2a2ac462113455d0fb
Gustavo <gustavonew2011@gmail.com>
2021-03-18 19:56:46 -0300

    2

--- Moeda switch/Moeda/moeda2.js
+++ Moeda switch/Moeda/moeda2.js
@@ -1,57 +1,47 @@
-var resultadoTotal
+var resultadoTotal;
 function conversao(){
+	    var valores = document.getElementById('valor').value;
 		var conversaoM = document.getElementById('option').value;
 		var resultadoConversao = document.getElementById('option2').value;
 
 
-		if (conversaoM == 1){
-		var moeda = 1
-		}
-
-		else if (conversaoM == 2){
-		var moeda = 5.53577
+		if (conversaoM == 2){
+		var moeda = valores / 5.53577
 		}
 
 		else if (conversaoM == 3){
-		var moeda = 4.45637
+		var moeda = valores / 4.45637
 		}
 
 		else if (conversaoM == 4){
-		var moeda = 6.60912
+		var moeda = valores / 6.60912
 		}
 
 		else if (conversaoM == 5){
-		var moeda = 7.72198
+		var moeda = valores / 7.72198
 		}
 
 		else{
-		var moeda = 0.06065
+		var moeda = valores / 0.06065
 		}
 
 		console.log(moeda)
 
-		if (resultadoConversao === 1){
-        var resultadoTotal = (moeda / 1)
+		if (resultadoConversao == 2){
+		var resultadoTotal = moeda / 5.53577
 		}
 
-		else if (resultadoConversao === 2){
-		var resultadoTotal = (moeda / 5.53577)
+		else if (resultadoConversao == 3){
+		var resultadoTotal = moeda / 4.45637
 		}
 
-		else if (resultadoConversao === 3){
-		var resultadoTotal = (moeda / 4.45637)
+		else if (resultadoConversao == 4){
+		var resultadoTotal = moeda / 6.63
 		}
 
-		else if (resultadoConversao === 4){
-		var resultadoTotal = (moeda / 6.60912)
+		else if (resultadoConversao == 5){
+		var resultadoTotal = moeda / 7.72198
 		}
-
-		else if (resultadoConversao === 5){
-		var resultadoTotal = (moeda / 7.72198)
-		}
-
-		else{
-		var resultadoTotal = (moeda / 0.06065)
-		}
+		console.log(moeda)
+		console.log(resultadoTotal)
 	}
-
