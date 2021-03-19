d1c5fe0363b695e72727a19efed810f498aa530b
Gustavo <gustavonew2011@gmail.com>
2021-03-19 00:32:25 -0300

    fixbug pagina de conversao

--- PROJETO-18-03-14-23h/js/moeda.js
+++ PROJETO-18-03-14-23h/js/moeda.js
@@ -3,6 +3,52 @@ function conversao() {
     var valores2 = document.getElementById('option2').value
     var input_texto = document.getElementById('valor').value
 
+    if (valores == 5.584 && valores2 == 0.0179){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 4.496 && valores2 == 0.0222){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 1 && valores2 == 1){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 6.733 && valores2 == 0.148){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 7.854 && valores2 == 0.127){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 0.0607 && valores2 == 16.666){
+    alert ("As moedas precisam ser diferentes")
+    location.reload();
+    return
+    }
+
+    if (valores == 4.496 && valores2 == 0.0179 ){
+    resultado = (valores * valores2 * input_texto * 10)
+    return 
+    }
+
+    if (valores == 1 && valores2 == 0.0179 ){
+    resultado = (valores * valores2 * input_texto * 10)
+    return 
+    }
+
     resultado = (valores * valores2 * input_texto)
     console.log(resultado)
     console.log(valores)
