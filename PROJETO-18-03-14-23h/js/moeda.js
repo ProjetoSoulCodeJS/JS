005cb20f6c4da9a7b947faed46e71a6defb70953
Amanda Borim <amandafborim@gmail.com>
2021-03-18 23:19:38 -0300

    Add files via upload

---
+++ PROJETO-18-03-14-23h/js/moeda.js
@@ -0,0 +1,18 @@
+function conversao() {
+    var valores = document.getElementById('option').value
+    var valores2 = document.getElementById('option2').value
+    var input_texto = document.getElementById('valor').value
+
+    resultado = (valores * valores2 * input_texto)
+    console.log(resultado)
+    console.log(valores)
+    console.log(input_texto)
+
+    // document.getElementById('valorFinal').value = resultado
+    // document.getElementById("valorFinal").value = resultado.toFixed(2)
+}
+
+
+function result(){
+    document.getElementById('valorFinal').innerHTML = resultado.toFixed(2)
+    }
