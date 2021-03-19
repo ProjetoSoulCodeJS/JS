d432fa3efacf7ff25c54480b739a72f4be905726
Gustavo <gustavonew2011@gmail.com>
2021-03-18 22:24:55 -0300

    2

--- Moeda switch/Moeda/moeda2.js
+++ Moeda switch/Moeda/moeda2.js
@@ -1,47 +1,15 @@
-var resultadoTotal;
+
+
 function conversao(){
-	    var valores = document.getElementById('valor').value;
-		var conversaoM = document.getElementById('option').value;
-		var resultadoConversao = document.getElementById('option2').value;
+       var valores = document.getElementById('option').value
+	   var valores2 = document.getElementById('option2').value
+	   var input_texto = document.getElementById('valor').value
 
+     resultado = (valores * valores2 * input_texto)
+	 console.log(resultado)
+	 console.log(valores)
+	 console.log(input_texto)
 
-		if (conversaoM == 2){
-		var moeda = valores / 5.53577
-		}
+	 document.getElementById('valorFinal').value = resultado
 
-		else if (conversaoM == 3){
-		var moeda = valores / 4.45637
-		}
-
-		else if (conversaoM == 4){
-		var moeda = valores / 6.60912
-		}
-
-		else if (conversaoM == 5){
-		var moeda = valores / 7.72198
-		}
-
-		else{
-		var moeda = valores / 0.06065
-		}
-
-		console.log(moeda)
-
-		if (resultadoConversao == 2){
-		var resultadoTotal = moeda * valor * 5.53577
-		}
-
-		else if (resultadoConversao == 3){
-		var resultadoTotal = moeda / 4.45637
-		}
-
-		else if (resultadoConversao == 4){
-		var resultadoTotal = moeda * valor * 6.60912
-		}
-
-		else if (resultadoConversao == 5){
-		var resultadoTotal = moeda / 7.72198
-		}
-		console.log(moeda)
-		console.log(resultadoTotal)
 	}
